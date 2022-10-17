7c9705f5f3b85e1a82021e83871faa97f523bf1d
salahulbain <salahul.bain@gmail.com>
2022-10-17 12:05:49 +0700

    first initial commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'src/navigation_controls.dart';
+import 'src/web_view_stack.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  final controller = Completer<WebViewController>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tour Guide'),
+        backgroundColor: Colors.orange,
+        actions: [
+          NavigationControls(controller: controller),
+        ],
+      ),
+      body: WebViewStack(controller: controller),
+    );
+  }
+}
